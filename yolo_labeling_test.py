942d68a52aec37e57b8957c0a75bdebbd51782d1
d <dj@example.com>
2020-05-03 23:35:21 -0500

    dataloader to label tensor

--- yolo_labeling_test.py
+++ yolo_labeling_test.py
@@ -1,5 +1,11 @@
 from yolo_labeling import *
 import test_data
+import numpy as np
+from provided_materials.code.data_helper import UnlabeledDataset,LabeledDataset
+from provided_materials.code.helper import collate_fn
+import torch
+import torchvision
+
 
 def test_corner_to_anchor():
     # simple box
@@ -16,15 +22,42 @@ def test_corner_to_anchor():
         and result.width == (18-5))
     print('passed test_corner_to_anchor')
 
-def load_real_data():
-	annot_scene0_lines = test_data.first_scene
+# def load_real_data():
+# 	annot_scene0_lines = test_data.first_scene
 
-	result = annotation_to_yolo_label(annot_scene0_lines, 3)
-	print(result)
+# 	result = annotation_to_yolo_label(annot_scene0_lines, 3)
+# 	print(result)
+
+def run_w_dataloader():
+	labeled_scene_index = np.arange(106, 134)
+	dataset = LabeledDataset(image_folder='provided_materials/data',
+	                         annotation_file='provided_materials/data/annotation.csv',
+	                         scene_index=labeled_scene_index,
+	                         transform=torchvision.transforms.ToTensor(),
+	                         extra_info=True)
+	loader = torch.utils.data.DataLoader(dataset,
+	                                     batch_size=4,
+	                                     shuffle=True,
+	                                     num_workers=2,
+	                                     collate_fn=collate_fn)
+	sample, targets, road_image, extra=iter(loader).next()
+	import pdb
+
+
+	for target in targets:
+		cur_res = dl_target_tuple_as_yolo_tensor(target)
+		# print(cur_res)
+		if len(target) > 0:
+			# there should at least be some non zero elements of grid
+			assert(sum(sum(sum(cur_res))) > 0)
+	# pdb.set_trace()
+	print('pased run_w_dataloader')
+
 
 def main():
 	test_corner_to_anchor()
-	load_real_data()
+#	load_real_data()
+	run_w_dataloader()
 
 if __name__ == '__main__':
 	main()
